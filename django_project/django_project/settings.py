5583388a81f5632dedff32bb03700f01b195333d
Sam Crabtree <samuel.crabtree@protonmail.com>
2023-05-23 13:34:33 -0400

    Chapter 10 Completed
    -Bootstrap5 Added
    -Crispy Forms Configured

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig'
+    # 3rd Party Apps
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local Apps
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
